19dd188b571a29df1f666f4f49e264efb508ca77
ArtusU <artusrock@hotmail.com>
2020-08-13 11:22:44 +0100

    Dynamic URL Routing & Customer Template

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -7,14 +7,18 @@ def home(request):
     customers = Customer.objects.all()
 
     total_customers = customers.count()
-
     total_orders = orders.count()
+    
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
 
-    context = {'orders': orders, 'customers': customers,
-                'total_orders': total_orders, 'delivered': delivered,
-                'pending': pending}
+    context = {
+        'orders': orders, 
+        'customers': customers,
+        'total_orders': total_orders, 
+        'delivered': delivered,
+        'pending': pending
+        }
 
     return render(request, 'accounts/dashboard.html', context)
 
@@ -23,8 +27,14 @@ def products(request):
 
     return render(request, 'accounts/products.html', {'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer':customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
 
 
 
