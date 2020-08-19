6aab2993a67df8030ee8829b346ad6a4b0e6bb09
ArtusU <artusrock@hotmail.com>
2020-08-19 11:16:25 +0100

    uploading images to the model

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib import messages
 
 
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import *
 
@@ -108,7 +108,15 @@ def userPage(request):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['customer'])
 def accountSettings(request):
-    context = {}
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
     return render(request, 'accounts/account_settings.html', context)
 
 
