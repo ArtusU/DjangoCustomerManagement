18ba1b45c93753756ea6a36b2e9ea5b85d0d1677
ArtusU <artusrock@hotmail.com>
2020-08-20 06:30:31 +0100

    signals

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django_filters',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
